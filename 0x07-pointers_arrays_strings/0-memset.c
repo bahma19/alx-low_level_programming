a2d59ab90fa313b86bf7115338991ff8e8372d5f
Bemnet Agegnehu <bemnetismyname19@gmail.com>
2023-10-23 03:51:55 -0700

    1

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+void char *_memset(char *s, char b, unsigned int n)
+{
+
+}
