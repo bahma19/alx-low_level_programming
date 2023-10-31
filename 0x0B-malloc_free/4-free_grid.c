c50f4bf27efe4db186d68ae428d070d2029933d8
Bemnet Agegnehu <bemnetismyname19@gmail.com>
2023-10-31 04:05:11 -0700

    1

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,54 +1,22 @@
 #include "main.h"
-#include <stdlib.h>
-#include <stdio.h>
+#include <stdlib>
 
 /**
- * argstostr - convert the params passed to the program to string
- * @ac: the argument count
- * @av: the argument vector
+ * free_grid - ...
+ * @grid: ...
+ * @height: ...
  *
  * Return: ...
  */
 
-char *argstostr(int ac, char **av)
+void free_grid(int **grid, int height)
 {
-	int ch = 0, i = 0, j = 0, k = 0;
-	char *s;
+	int i;
 
-	if (ac == 0 || av == NULL)
-		return (NULL);
-	while (i < ac)
+	for (i = 0; i < height; i++)
 	{
-		while (av[i][j])
-		{
-			ch++;
-			j++;
-		}
-
-		j = 0;
-		i++;
+		free(grid[i]);
 	}
 
-	s = malloc((sizeof(char) * ch) + ac + 1);
-
-	i = 0;
-	while (av[i])
-	{
-		while (av[i][j])
-		{
-			s[k] = av[i][j];
-			k++;
-			j++;
-		}
-
-		s[k] = '\n';
-
-		j = 0;
-		k++;
-		i++;
-	}
-
-	k++;
-	s[k] = '\0';
-	return (s);
+	free(grid);
 }
